65730288f1e5f16fd0b3014162b325fbdf450900
Owen Prime <oprime@sajari.com>
2015-04-27 22:14:28 +1000

    Support older iWorks files by reading the embedded Preview.pdf file #8

--- pages.go
+++ pages.go
@@ -40,8 +40,11 @@ func ConvertPages(input io.Reader) (string, map[string]string) {
 			archiveInfoData, err := ioutil.ReadAll(io.LimitReader(bReader, archiveLength))
 			archiveInfo := &TSP.ArchiveInfo{}
 			err = proto.Unmarshal(archiveInfoData, archiveInfo)
-
 			fmt.Println("archiveInfo:", archiveInfo, err)
+		} else if f.Name == "QuickLook/Preview.pdf" {
+			rc, _ := f.Open()
+			defer rc.Close()
+			return ConvertPdf(rc)
 		}
 	}
 
