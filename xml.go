cf2e37efda0aadfc69f00c755c33ff85075df327
David Howden <dhowden@gmail.com>
2015-08-09 12:06:37 +1000

    Tidied up XML handling.

--- xml.go
+++ xml.go
@@ -12,38 +12,51 @@ func ConvertXml(input io.Reader) (string, map[string]string) {
 	meta := make(map[string]string)
 	cleanXml, err := Tidy(input, true)
 	if err != nil {
+		// TODO: Return error
 		log.Println("Tidy:", err)
 	}
 	return Xml2Text(bytes.NewReader(cleanXml), []string{}, []string{}, true), meta
 }
 
 // Convert XML to plain text given how to treat elements
-func Xml2Text(input io.Reader, breaks []string, skip []string, strict bool) string {
-	var output string
-	var depth int
+func Xml2Text(r io.Reader, breaks []string, skip []string, strict bool) string {
+	var result string
 
-	x := xml.NewDecoder(input)
-	x.Strict = strict
-	for d, err := x.Token(); d != nil && err == nil; d, err = x.Token() {
-		switch v := d.(type) {
+	dec := xml.NewDecoder(r)
+	dec.Strict = strict
+	for {
+		t, err := dec.Token()
+		if err != nil {
+			// TODO: Handle non io.EOF error
+			break
+		}
+
+		switch v := t.(type) {
 		case xml.CharData:
-			output += string(v)
+			result += string(v)
 		case xml.StartElement:
 			for _, breakElement := range breaks {
 				if v.Name.Local == breakElement {
-					output += "\n"
+					result += "\n"
 				}
 			}
 			for _, skipElement := range skip {
 				if v.Name.Local == skipElement {
-					depth = 1
-					for d, err := x.Token(); d != nil && err == nil; d, err = x.Token() {
-						switch d.(type) {
+					depth := 1
+					for {
+						t, err := dec.Token()
+						if err != nil {
+							// TODO: Handle non io.EOF error
+							break
+						}
+
+						switch t.(type) {
 						case xml.StartElement:
 							depth++
 						case xml.EndElement:
 							depth--
 						}
+
 						if depth == 0 {
 							break
 						}
@@ -52,21 +65,26 @@ func Xml2Text(input io.Reader, breaks []string, skip []string, strict bool) stri
 			}
 		}
 	}
-	return output
+	return result
 }
 
 // Convert XML to a nested string map
-func Xml2Map(input io.Reader) map[string]string {
-	output := make(map[string]string)
-	x := xml.NewDecoder(input)
+func Xml2Map(r io.Reader) map[string]string {
+	m := make(map[string]string)
+	dec := xml.NewDecoder(r)
 	var tagName string
-	for d, err := x.Token(); d != nil && err == nil; d, err = x.Token() {
-		switch v := d.(type) {
+	for {
+		t, err := dec.Token()
+		if err != nil {
+			break
+		}
+
+		switch v := t.(type) {
 		case xml.StartElement:
 			tagName = string(v.Name.Local)
 		case xml.CharData:
-			output[tagName] = string(v)
+			m[tagName] = string(v)
 		}
 	}
-	return output
+	return m
 }
