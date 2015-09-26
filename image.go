9ecc1d7b21a4c1e90175391f94bf8e44fbeea960
David Howden <dhowden@gmail.com>
2015-09-26 11:05:09 +1000

    Added build tag 'ocr' to isolate gosseract dependency.
    To build docconv with OCR (ConvertImage) support, add the build tag ocr.

---
+++ image.go
@@ -0,0 +1,12 @@
+// build !ocr
+
+package docconv
+
+import (
+	"fmt"
+	"io"
+)
+
+func ConvertImage(r io.Reader) (string, map[string]string, error) {
+	return "", nil, fmt.Errorf("docconv not built with `ocr` build tag")
+}
--- image_ocr.go
+++ image_ocr.go
@@ -1,3 +1,5 @@
+// +build ocr
+
 package docconv
 
 import (
