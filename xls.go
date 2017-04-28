3f3bf554aae9d105fc365ad4d2e099d4a5c6452b
Felipe Oliveira <felipeweb.programador@gmail.com>
2017-04-28 18:47:42 -0300

    fix xls temp file and remove final \n

--- xls.go
+++ xls.go
@@ -3,32 +3,41 @@ package docconv
 import (
 	"fmt"
 	"io"
+	"io/ioutil"
+	"os"
 	"strings"
+	"time"
 
 	"github.com/extrame/xls"
 )
 
 // ConvertXLS Convert MS Excel Spreadsheet
 func ConvertXLS(r io.Reader) (string, map[string]string, error) {
-	f, err := NewLocalFile(r, "/tmp", "sajari-convert-")
+	f, err := ioutil.TempFile("", "nuveo")
 	if err != nil {
-		return "", nil, fmt.Errorf("error creating local file: %v", err)
+		return "", nil, fmt.Errorf("error on create temp file: %v", err)
 	}
-	defer f.Done()
+	defer os.Remove(f.Name())
 
-	fileStat, err := f.Stat()
+	var byt []byte
+	byt, err = ioutil.ReadAll(r)
 	if err != nil {
-		return "", nil, fmt.Errorf("error on getting file stats: %v", err)
+		return "", nil, fmt.Errorf("error on readAll: %v", err)
 	}
-	w, err := xls.OpenReader(f, "utf-8")
+	_, err = f.Write(byt)
+	if err != nil {
+		return "", nil, fmt.Errorf("error on write temp file: %v", err)
+	}
+
+	_ = f.Close()
+
+	w, err := xls.Open(f.Name(), "utf-8")
 	if err != nil {
 		return "", nil, fmt.Errorf("error on xls parsing: %v", err)
 	}
-
 	// Meta data
 	meta := make(map[string]string)
-	meta["ModifiedDate"] = fmt.Sprintf("%d", fileStat.ModTime().Unix())
-
+	meta["ModifiedDate"] = fmt.Sprintf("%d", time.Now().Unix())
 	// Document body
 	var body string
 	for i := 0; i < w.NumSheets(); i++ {
@@ -39,13 +48,11 @@ func ConvertXLS(r io.Reader) (string, map[string]string, error) {
 				col := row.Col(k)
 				r = append(r, col)
 			}
-
 			body += strings.Join(r[:], ",")
 			if j < int(w.GetSheet(i).MaxRow) {
 				body += "\n"
 			}
 		}
 	}
-
-	return body, meta, nil
+	return strings.TrimSuffix(body, "\n"), meta, nil
 }
