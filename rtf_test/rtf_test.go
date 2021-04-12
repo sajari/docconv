6b8d2b5d6697f69c9ef632e0b48a171ed84ca9de
remLse <54984957+remLse@users.noreply.github.com>
2021-04-12 11:49:41 +0200

    rtf: don't ignore lines less than 5 characters long (#91)
    It looks like the original author of this function intended to do a bounds check
    on the line variable before checking if it has the `### ` prefix. Instead, we can
    just use `strings.HasPrefix` which also means that shorter lines are now 
    included in the output.
    
    Co-authored-by: Remo Senekowitsch <remo.senekowitsch@icloud.com>

---
+++ rtf_test/rtf_test.go
@@ -0,0 +1,38 @@
+package rtf_test
+
+import (
+	"bytes"
+	"io/ioutil"
+	"strings"
+	"testing"
+
+	"code.sajari.com/docconv"
+)
+
+func TestConvertRTF(t *testing.T) {
+	data, err := ioutil.ReadFile("testdata/test.rtf")
+	if err != nil {
+		t.Fatalf("got error %v, want nil", err)
+	}
+	res, _, err := docconv.ConvertRTF(bytes.NewReader(data))
+	if err != nil {
+		t.Fatalf("got error %v, want nil", err)
+	}
+	lines := strings.Split(res, "\n")[2:]
+	line, expectedLine := lines[0], "hello world"
+	if line != expectedLine {
+		t.Fatalf("got %s, want %s", line, expectedLine)
+	}
+	line, expectedLine = lines[1], "helo"
+	if line != expectedLine {
+		t.Fatalf("got %s, want %s", line, expectedLine)
+	}
+	line, expectedLine = lines[2], ""
+	if line != expectedLine {
+		t.Fatalf("got %s, want %s", line, expectedLine)
+	}
+	line, expectedLine = lines[3], "1"
+	if line != expectedLine {
+		t.Fatalf("got %s, want %s", line, expectedLine)
+	}
+}
