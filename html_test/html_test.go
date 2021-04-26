f60d9d3ccc8740cd61683583df53243816aa6c6f
Jonathan Ingram <jingram@sajari.com>
2021-04-26 14:22:59 +1000

    update test for TestConvertHTML
    Use table tests with current package value "", and "good".
    
    Also, just compare the expected string instead of comparing line by
    line.

--- html_test/html_test.go
+++ html_test/html_test.go
@@ -2,40 +2,59 @@ package html_test
 
 import (
 	"bytes"
+	"fmt"
 	"io/ioutil"
-	"strings"
 	"testing"
+
+	"github.com/google/go-cmp/cmp"
 
 	"code.sajari.com/docconv"
 )
 
-func TestConvertHTML(t *testing.T) {
+func TestConvertHTML_readabilityUseClasses(t *testing.T) {
+	tests := []struct {
+		readabilityUseClasses string
+		want                  string
+	}{
+		// The default value set by this package.
+		{
+			readabilityUseClasses: "",
+			want:                  ``,
+		},
+		// Enable output with readability true.
+		{
+			readabilityUseClasses: "good",
+			want: `1
+word
+This is a full sentence.
+`,
+		},
+	}
 
-	// enable output with readability: true
-	docconv.HTMLReadabilityOptionsValues.ReadabilityUseClasses = "good"
+	for _, tt := range tests {
+		t.Run(fmt.Sprintf("%q", tt.readabilityUseClasses), func(t *testing.T) {
+			old := docconv.HTMLReadabilityOptionsValues.ReadabilityUseClasses
+			t.Cleanup(func() {
+				docconv.HTMLReadabilityOptionsValues.ReadabilityUseClasses = old
+			})
+			docconv.HTMLReadabilityOptionsValues.ReadabilityUseClasses = tt.readabilityUseClasses
 
-	data, err := ioutil.ReadFile("testdata/test.html")
+			data, err := ioutil.ReadFile("testdata/test.html")
+			must(t, err)
+			got, _, err := docconv.ConvertHTML(bytes.NewReader(data), true)
+			must(t, err)
+
+			diff := cmp.Diff(tt.want, got)
+			if diff != "" {
+				t.Errorf("result mismatch (-want +got):\n%v", diff)
+			}
+		})
+	}
+}
+
+func must(t *testing.T, err error) {
+	t.Helper()
 	if err != nil {
-		t.Fatalf("got error %v, want nil", err)
-	}
-	res, _, err := docconv.ConvertHTML(bytes.NewReader(data), true)
-	if err != nil {
-		t.Fatalf("got error %v, want nil", err)
-	}
-	lines := strings.Split(res, "\n")
-	for _, i := range lines {
-		t.Log(i)
-	}
-	line, expectedLine := lines[0], "1"
-	if line != expectedLine {
-		t.Fatalf("got %s, want %s", line, expectedLine)
-	}
-	line, expectedLine = lines[1], "word"
-	if line != expectedLine {
-		t.Fatalf("got %s, want %s", line, expectedLine)
-	}
-	line, expectedLine = lines[2], "This is a full sentence."
-	if line != expectedLine {
-		t.Fatalf("got %s, want %s", line, expectedLine)
+		t.Fatal(err)
 	}
 }
