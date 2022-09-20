f48142557c40e781d91f84b6d13ef6fef58a42c0
Jonathan Ingram <jingram@sajari.com>
2022-09-20 17:36:18 +1000

    docd: return JSON errors when things fail (#127)

--- docd/main.go
+++ docd/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"encoding/json"
 	"flag"
 	"fmt"
 	"log"
@@ -53,6 +52,10 @@ func main() {
 		}
 	}
 
+	cs := &convertServer{
+		er: er,
+	}
+
 	// TODO: Improve this (remove the need for it!)
 	docconv.HTMLReadabilityOptionsValues = docconv.HTMLReadabilityOptions{
 		LengthLow:             *readabilityLengthLow,
@@ -73,73 +76,13 @@ func main() {
 		return
 	}
 
-	serve(er)
-}
-
-func convert(w http.ResponseWriter, r *http.Request) {
-	// Readability flag. Currently only used for HTML
-	var readability bool
-	if r.FormValue("readability") == "1" {
-		readability = true
-		if *logLevel >= 2 {
-			log.Println("Readability is on")
-		}
-	}
-
-	path := r.FormValue("path")
-	if path != "" {
-		b, err := docconv.ConvertPathReadability(path, readability)
-		if err != nil {
-			// TODO: return a sensible status code for errors like this.
-			log.Printf("error converting path '%v': %v", path, err)
-			return
-		}
-		w.Write(b)
-		return
-	}
-
-	// Get uploaded file
-	file, info, err := r.FormFile("input")
-	if err != nil {
-		log.Println("File upload", err)
-		return
-	}
-	defer file.Close()
-
-	// Abort if file doesn't have a mime type
-	if len(info.Header["Content-Type"]) == 0 {
-		log.Println("No content type", info.Filename)
-		return
-	}
-
-	// If a generic mime type was provided then use file extension to determine mimetype
-	mimeType := info.Header["Content-Type"][0]
-	if mimeType == "application/octet-stream" {
-		mimeType = docconv.MimeTypeByExtension(info.Filename)
-	}
-
-	if *logLevel >= 1 {
-		log.Println("Received file: " + info.Filename + " (" + mimeType + ")")
-	}
-
-	data, err := docconv.Convert(file, mimeType, readability)
-	if err != nil {
-		log.Printf("error converting data: %v", err)
-		data = &docconv.Response{
-			Error: err.Error(),
-		}
-	}
-
-	if err := json.NewEncoder(w).Encode(data); err != nil {
-		log.Printf("error marshaling JSON data: %v", err)
-		return
-	}
+	serve(er, cs)
 }
 
 // Start the conversion web service
-func serve(er internal.ErrorReporter) {
+func serve(er internal.ErrorReporter, cs *convertServer) {
 	r := mux.NewRouter()
-	r.HandleFunc("/convert", convert)
+	r.HandleFunc("/convert", cs.convert)
 
 	// Start webserver
 	log.Println("Setting log level to", *logLevel)
